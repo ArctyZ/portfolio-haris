72ea8d9f6a523c3ebb9e55978bc498e8973019f2
Haris <harisbaraja56@gmail.com>
2025-01-08 11:01:51 +0700

    feat:about almost done

---
+++ src/components/Contact.tsx
@@ -0,0 +1,39 @@
+import { Link } from "react-router";
+import { IoIosMail } from "react-icons/io";
+import { FiGithub, FiLinkedin, FiFileText } from "react-icons/fi";
+
+export default function Contact() {
+  return (
+    <div className=" relative border-[1px] border-white w-[75%] m-auto">
+      <h1 className="absolute -top-4 left-2 text-white text-xl bg-black">
+        Information
+      </h1>
+      <div className="flex md:flex-row flex-col  gap-5 p-5 w-full items-center justify-between">
+        <div>
+          <Link to="mailto:harisbaraja56@gmail.com" className="flex flex-col gap-2 items-center p-3" target="_blank">
+            <IoIosMail size={50} />
+            <span className="text-xl">Email</span>
+          </Link>
+        </div>
+        <div>
+          <Link to="https://github.com/ArctyZ" className="flex flex-col gap-2 items-center p-3" target="_blank">
+            <FiGithub size={50} />
+            <span className="text-xl">Github</span>
+          </Link>
+        </div>
+        <div>
+          <Link target="_blank" to="https://www.linkedin.com/in/muhammad-harits-b01b91218/" className="flex flex-col gap-2 items-center p-3">
+            <FiLinkedin size={50} />
+            <span className="text-xl">LinkedIn</span>
+          </Link>
+        </div>
+        <div>
+          <Link target="_blank" to="https://drive.google.com/file/d/1dZVQxh5NIaw4kUcxnUgY1xL_hBR79Nge/view?usp=drive_link" className="flex flex-col gap-2 items-center p-3">
+            <FiFileText size={50} />
+            <span className="text-xl">Resume</span>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,34 +1,23 @@
-import TechStack from "../components/TechStack"
-import { FaCircle } from "react-icons/fa";
+import Contact from "../components/Contact";
+
 export default function About() {
   return (
-    <div>
-      {/* Title */}
-      <div className="flex justify-center items-center text-center text-4xl font-bold my-5 text-white">
-        <h1>About Myself</h1>
-      </div>
-      {/* Description */}
-      <div>
-        <p className="text-center text-white">
-          I am a Web Developer with a passion for creating beautiful and functional
-          websites. I have a strong foundation in HTML, CSS and JavaScript, and
-          have experience working with frameworks such as React and Next.js. I am
-          also experienced in working with databases such as MongoDB and MySQL.
-          Currently I am learning Php and Laravel and I am also learning computer science to better understand how a computer works.
-        </p>
-      </div>
-      <div className="text-white my-5">
-        <h2>Some of the things that I can do:</h2>
-        <ul>
-          <li className="flex flex-row items-center gap-1"><FaCircle/><span>Create a company profile website</span></li>
-          <li className="flex flex-row items-center gap-1"><FaCircle/><span>Create an E-Commerce website</span></li>
-          <li className="flex flex-row items-center gap-1"><FaCircle/><span>Web Scraping</span></li>
-          <li className="flex flex-row items-center gap-1"><FaCircle/><span>Web Scraping</span></li>
-        </ul>
-      </div>
-      <div className="mt-5">
-        <TechStack/>
-      </div>
-    </div>
+     <div className="flex flex-col gap-5  my-5">
+          <h1 className="text-3xl text-center">About Me</h1>
+          <div className="text-center text-xl mb-3">
+            Hi, I'm Haris Baraja, a passionate and dedicated Full-Stack Web Developer who loves turning ideas into functional, user-friendly digital experiences.
+            <br/><br />
+            While I may not have formal work experience yet, I have developed a strong foundation in web development trhough self-learning and hands-on projects.
+            My skills include front-end technologies like HTML, CSS, JavaScript, ReactJs, and back-end frameworks like NodeJs, Express and MySQL.
+            <br /> <br />
+            In my free time, I enjoy learning about computers in general, game development and learning new things.
+            <br /> <br />
+            I'm exited to connect with others in the tech community and contribute to impactful projects. Lets build something amazing together! 
+            <br /><br />
+          </div>
+          <div>
+           <Contact />
+          </div>
+        </div>
   )
 }
